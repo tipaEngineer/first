38c552906994bf92a5d095f19cce8fa665261dd7
tipaproger <lancer5610@gmail.com>
2022-04-10 19:13:04 +0300

    All app commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import {App} from './App';
+import {PRODUCTS} from "./index";
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App products={PRODUCTS}/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,106 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+interface Product {
+    category: string;
+    price: string;
+    stocked: boolean;
+    name: string;
 }
 
-export default App;
+interface AllProducts {
+    products: Array<Product>;
+};
+
+export const App: React.FC<AllProducts> = ({products}) => {
+    const [searchText, setText] = useState("")
+    const [isOnlyStoked, setOnlyStocked] = useState(false)
+
+    const textProperties = {searchText, setText, isOnlyStoked, setOnlyStocked}
+
+    return (
+        <>
+            <SearchBar textProperties={textProperties}/>
+            <ProductTable products={products} isOnlyStoked={isOnlyStoked} searchText={searchText}/>
+        </>
+    )
+}
+const ProductRow: React.FC<{ product: Product }> = (product) => {
+    const name = product.product.stocked ? product.product.name :
+        <span style={{color: "red"}}>{product.product.name}</span>
+    return (<>
+            <tr>
+                <td>{name}</td>
+                <td>{product.product.price}</td>
+            </tr>
+        </>
+    )
+}
+const ProductCategoryRow: React.FC<{category:string}> = (category) => {
+    return(<>
+            <tr>
+                <th colSpan={2}>
+                    {category.category}
+                </th>
+            </tr>
+        </>
+    )
+}
+
+const ProductTable: React.FC<{products:Product[],searchText:string; isOnlyStoked:boolean}> = ({products,searchText,isOnlyStoked}) => {
+    // @ts-ignore
+    const rows: any | null | undefined = []
+    let lastCategory = ""
+    products.forEach((prod)=>{
+        if(prod.name.indexOf(searchText)=== -1){
+            return
+        }
+        if(isOnlyStoked && !prod.stocked){
+            return
+        }
+        if(prod.category!==lastCategory){
+            rows.push(<ProductCategoryRow
+                category={prod.category}
+                key={prod.category} />)
+        }
+        rows.push(<ProductRow product={prod}/>)
+        lastCategory = prod.category
+
+    })
+    return (<>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
+    </>)
+}
+
+
+
+const SearchBar: React.FC<{textProperties:{searchText: string; setText: React.Dispatch<React.SetStateAction<string>>; isOnlyStoked:boolean; setOnlyStocked: React.Dispatch<React.SetStateAction<boolean>>}}> = (properties) => {
+
+    const setSearchText = (event: React.ChangeEvent<HTMLInputElement>) => {
+        properties.textProperties.setText(event.target.value)
+        console.log(properties.textProperties.searchText)
+    }
+    const setIsOnlyStocked = (event: React.ChangeEvent<HTMLInputElement>) => {
+        properties.textProperties.setOnlyStocked(!properties.textProperties.isOnlyStoked)
+    }
+
+    return (
+        <>
+            <input type={"text"} placeholder={"Search..."} value={properties.textProperties.searchText} onChange={setSearchText}/>
+            <p><label><input type={"checkbox"} onChange={setIsOnlyStocked}/>Only show products in stock</label>
+            </p>
+        </>
+    )
+}
+
+
