edad72938464a3005fde1c5e19c5a5a027cf85e7
tipaproger <lancer5610@gmail.com>
2022-04-15 14:57:09 +0300

    added fetch data

--- src/users.tsx
+++ src/users.tsx
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+interface UsersData {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: {
+            lat: string;
+            lng: string;
+        };
+    };
+    phone: string;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+    };
+}
 
 export const Users: React.FunctionComponent = () => {
-    return <div>ppppp</div>;
+    const [data, setData] = useState<Array<UsersData>>();
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    fetch(url)
+        .then((r) => r.json())
+        .then((j) => setData(j));
+    // eslint-disable-next-line no-undef
+    const aray: Array<JSX.Element> = [];
+    data?.forEach((e) => {
+        aray.push(<div>{e.name}</div>);
+    });
+    return <div>{aray}</div>;
 };
