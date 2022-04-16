5b48003c12ce439e097d1e2b6813fdf586836ffe
tipaproger <lancer5610@gmail.com>
2022-04-16 13:32:44 +0300

    added fetching user data and rendering it

--- src/users.tsx
+++ src/users.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface UsersData {
-    id: number;
+    id: string;
     name: string;
     username: string;
     email: string;
@@ -26,14 +26,70 @@ interface UsersData {
 
 export const Users: React.FunctionComponent = () => {
     const [data, setData] = useState<Array<UsersData>>();
-    const url = 'https://jsonplaceholder.typicode.com/users';
-    fetch(url)
-        .then((r) => r.json())
-        .then((j) => setData(j));
+    const [user, setUser] = useState<UsersData>();
+    const [userId, setUserID] = useState('');
+
+    const url = 'https://jsonplaceholder.typicode.com/users/';
+
+    //if changing userId => url also changing;
+
+    useEffect(() => {
+        let newUrl = url;
+        fetch(!userId ? newUrl : newUrl.concat(userId))
+            .then((r) => r.json())
+            .then((j) => (!userId ? setData(j) : setUser(j)))
+            .catch((e) => alert(e));
+    }, [userId]);
     // eslint-disable-next-line no-undef
-    const aray: Array<JSX.Element> = [];
-    data?.forEach((e) => {
-        aray.push(<div>{e.name}</div>);
+    const renderingArray: Array<JSX.Element> = [];
+    console.log(user ? true : false);
+    data?.forEach((e, index) => {
+        renderingArray.push(
+            // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+            <div
+                style={{ display: 'table-row' }}
+                onClick={() => {
+                    setUserID(e.id);
+                }}
+            >
+                <div style={{ display: 'table-cell' }}>
+                    <td>{e.name}</td>
+                </div>
+                <div style={{ display: 'table-cell' }}>
+                    <td>{e.username}</td>
+                </div>
+            </div>,
+        );
+        // adding users data to render
+        if (user?.id == e.id) {
+            renderingArray.push(
+                // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+                <div
+                    style={{ display: 'table-row', background: 'gray' }}
+                    onClick={() => setUser(undefined)}
+                >
+                    <div style={{ display: 'table-cell' }}>
+                        <td>{user.email}</td>
+                    </div>
+                    <div style={{ display: 'table-cell' }}>
+                        <td>{user.phone}</td>
+                    </div>
+                </div>,
+            );
+        }
     });
-    return <div>{aray}</div>;
+    return (
+        <div>
+            <table>
+                <caption>Users</caption>
+                <thead>
+                    <tr>
+                        <th>name</th>
+                        <th>username</th>
+                    </tr>
+                </thead>
+                <tbody>{renderingArray}</tbody>
+            </table>
+        </div>
+    );
 };
